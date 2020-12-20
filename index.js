49f38082757d340e1525e5672e00a244644f5b38
xna <xna00@qq.com>
2020-12-20 17:11:30 +0800

    增加 列出所有仓库

--- index.js
+++ index.js
@@ -64,4 +64,35 @@ module.exports.deleteRepo = (repo) => {
     })
     req.end()
 }
+module.exports.listRepo = ()=>{
+    const formData = {
+        access_token: '5579d082f80f96be814a986f85f1e044',
+        sort: 'updated',
+        page: 1,
+        per_page: 100
+    }
+    const request = https.request({
+        hostname: 'gitee.com',
+        path:`/api/v5/user/repos?${querystring.stringify(formData)}`,
+        method:'GET',
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+        }, 
+    }, (response)=>{
+        console.log(response.statusCode);
+        const chunks = []
+        let data = ''
+        response.on('data',(chunk)=>{
+            chunks.push(chunk)
+        })
+        response.on('end',()=>{
+            data = Buffer.concat(chunks).toString()
+            data = JSON.parse(data)
+            data.forEach((repo)=>{
+                console.log(repo.path);
+            })
+        })
+    })
+    request.end()
+}
 module.exports.buildPages = require('./pages/build.js')
